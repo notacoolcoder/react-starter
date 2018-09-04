80d1d9dc2a3526f8886b32d0e30569b716609f3b
notacoolcoder <notacoolcoder@gmail.com>
2018-09-04 23:27:40 +0530

    skeleton added

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+constructor(props){
+  super(props)
+
+  
+}
+
+componentDidMount(){
+
+
+}
+
+
+  render() {
+
+   
+
+    return (
+      <div >
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Components/Dummy/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class extends Component {
+
+constructor(props){
+  super(props)
+    this.state={
+        
+    }
+  
+}
+
+componentDidMount(){
+
+
+}
+
+
+  render() {
+
+   
+
+    return (
+      <div >
+
+      </div>
+    );
+  }
+}
+
